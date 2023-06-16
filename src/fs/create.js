ad74e0bb90cfe4167c8f969227ed5f0c83510fe1
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-16 10:08:35 +0200

    [fs] write a function that creates a new file in the directory

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile, existsSync } from 'node:fs';
+
+const create = async (text) => {
+    const path = './files/fresh.txt';
+
+    if (existsSync(path)) {
+        throw new Error('FS operation failed');
+    } else {
+        writeFile(path, text, (err) => {
+        if(err) console.error(err);
+        });
+    }
+    
 };
 
-await create();
+await create('I am fresh and young');
