af7fd19d8390382d51c6561461a1cac65815afeb
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-16 11:02:23 +0200

    [fs] wrote a function that copies all the files from one directory into another directory

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
-const copy = async () => {
-    // Write your code here 
+import { existsSync, cp } from 'node:fs';
+
+const copy = async (dir) => {
+    const newDir = `${dir}_copy`;
+
+    if(!existsSync(dir)||existsSync(newDir)) {
+        throw new Error('FS operation failed');
+    } else {
+        cp(dir, newDir, { recursive: true }, (err) => {
+            if (err) {
+              console.error(err);
+            }
+        });
+    }
 };
 
-await copy();
+await copy('./files');
