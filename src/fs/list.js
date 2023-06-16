95fff134005547518a4dae159ff5bed7cd119df0
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-16 11:58:18 +0200

    [fs] wrote a function that lists a;; the files in a directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
-const list = async () => {
-    // Write your code here 
+import { existsSync, readdir } from 'node:fs';
+
+const list = async (folder) => {
+   if(!existsSync(folder)) {
+       throw new Error('FS operation failed');
+   } else {
+       readdir(folder, (err, files) => {
+            if (err) {
+                throw new Error(err);
+            } else {
+                console.log(files);
+            }
+       });
+   }
 };
 
-await list();
+await list('./files');
