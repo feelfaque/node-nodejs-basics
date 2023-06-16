ef1ceb056b70005b3d8c5c0fdd03281cbc11833c
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-16 11:44:12 +0200

    [fs] wrote a function that deletes a file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
-const remove = async () => {
-    // Write your code here 
+import { rm, existsSync } from 'node:fs';
+
+const remove = async (path) => {
+    if(!existsSync(path)) {
+        throw new Error('FS operation failed');
+    } else {
+        rm(path, (err) => {
+            if (err) throw new Error(err);
+        });
+    }
 };
 
-await remove();
+await remove('./files/fileToremove.txt');
