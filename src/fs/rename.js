d8a39265f99052cd9614b5b0fe885295f39106be
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-16 11:27:23 +0200

    [fs] wrote a function that renames a file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
-const rename = async () => {
-    // Write your code here 
+import { rename as renameFile, existsSync } from 'node:fs';
+
+const rename = async (oldPath, newPath) => {
+    if (!existsSync(oldPath)||existsSync(newPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        renameFile(oldPath, newPath, (err) => {
+            if(err) throw new Error(err);
+        });
+    }
 };
 
-await rename();
+await rename('./files/wrongFileName.txt', './files/properFileName.md');
