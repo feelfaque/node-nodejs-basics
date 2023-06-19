5cb1dbd981dd7fb1ca59eda530815452afb2d6d4
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-19 15:42:22 +0200

    [zip] wrote a function that compresses a file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { resolve } from 'node:path';
+import { createGzip } from 'node:zlib';
+
+const directory = resolve('src/zip/files')
+const filePath = resolve(`${directory}/fileToCompress.txt`);
+
+
 const compress = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(filePath);
+    const writableStream = createWriteStream(`${directory}/archive.gz`)
+
+    readableStream.pipe(createGzip())
+                .pipe(writableStream);
 };
 
 await compress();
