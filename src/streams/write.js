4b705b1cc20b512a6d161e9598936342318ec0c6
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-19 15:06:24 +0200

    [streams] wrote a function that creates a writable stream to a file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'node:fs';
+import { resolve } from 'node:path';
+
+const filePath = resolve('src/streams/files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(filePath, {encoding: "utf-8"});
+
+    process.stdin.on("data", data => {
+       stream.write(data);
+    })
 };
 
 await write();
